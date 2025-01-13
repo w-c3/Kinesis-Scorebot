1e11686f7e70fcb97074680a1474a90d6b9a227e
Matthew <mattkoco@icloud.com>
2025-01-13 09:28:42 -0500

    Update kinesis.sh

--- kinesis.sh
+++ kinesis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo " "
-echo "Kinesis Scorebot v1"
+echo "Kinesis Scorebot v2"
 echo "NOTE: Please allow up to 5 minutes for scorebot updates & injects."
 echo "Injects: NO" # Modify this if you run an inject
 
@@ -63,6 +63,17 @@ check_file_permissions() {
     actual_permissions=$(stat -c "%a" "$file")
     
     if [ "$actual_permissions" == "$expected_permissions" ]; then
+        echo "Vulnerability fixed: '$vuln_name'"
+    else
+        echo "Unsolved Vuln"
+    fi
+}
+
+check_file_ownership() { # Thanks Coyne <3
+    local file="$1"
+    local expected_owner="$2"
+    local vuln_name="$3"
+     if getfacl "$file" 2>/dev/null | grep -q "owner: $expected_owner"; then
         echo "Vulnerability fixed: '$vuln_name'"
     else
         echo "Unsolved Vuln"
